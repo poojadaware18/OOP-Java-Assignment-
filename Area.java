352ab1fc74e2afae7039ea1c7144dc4e41a0c3ce
poojadaware18 <dawarepooja18@gmail.com>
2025-07-30 21:04:33 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,24 @@
+import java.util.*;
+{
+public class Area
+{
+float h,b;
+float c ;
+float c = 0.5*b*h;
+System.out.println("The area of Triangle is"+c);
+{
+public static void main(String[] args)
+{
+Scanner s = new Scanner(System.in);
+System.out.println("Enter the h and b of Triangle");
+float H =s.nextInt();
+float B =s.nextInt();
+Area A =new Area();
+}
+}
+}
+}
+
+
+
+
