d098591aefdfd1c7260079395b4c97c25f434ef7
poojadaware18 <dawarepooja18@gmail.com>
2025-07-30 20:45:59 +0530

    Add files via upload

---
+++ Swapping.java
@@ -0,0 +1,14 @@
+public class Swapping
+{
+public static void main(String args[])
+{
+int a=10;
+int b=20;
+int temp;
+System.out.println("Before Swapping Value of a:"+a+"Value of b:"+b);
+temp=a;
+a=b;
+b=temp;
+System.out.println("After Swapping Value of a:"+a+"Value of b:"+b);
+}
+}
