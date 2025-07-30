984bb161cc4eb7e9ba80129893c9bcbb6dcccb57
poojadaware18 <dawarepooja18@gmail.com>
2025-07-30 20:58:01 +0530

    Add files via upload

---
+++ PositiveNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PositiveNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        double number = scanner.nextDouble();
+        
+        if (number > 0) {
+            System.out.println(number + " is a positive number.");
+        } else if (number < 0) {
+            System.out.println(number + " is a negative number.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+        
+        scanner.close();
+    }
+}
